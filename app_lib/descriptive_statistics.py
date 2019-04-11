3b4e28e439fb7e24e28bb4980c7651bea9968a42
Iman Ali <IAli@spartaglobal.com>
2019-04-11 07:12:27 +0100

    Finally done something in my main

--- app_lib/descriptive_statistics.py
+++ app_lib/descriptive_statistics.py
@@ -5,21 +5,48 @@ from app_lib.class_calculations import  *
 
 
 
-crime_data= CrimeFileReader("car_accidents.xlsx")
+crime_data = CrimeFileReader("car_accidents.xlsx")
 crime_data = crime_data.print_all_cell_data()
-
-
 calc = Calculations()
+print("--------------------------------------------------------------------------------------------------------")
+print("                                                 Descriptive Statistics ")
+print("--------------------------------------------------------------------------------------------------------")
 calc.average_number_of_casualities(crime_data)
+
+print("--------------------------------------------------------------------------------------------------------")
+print("                                                 User input ")
+print("--------------------------------------------------------------------------------------------------------")
+a = input("What gender do we think is most likely to be in accident")
+if a == "male":
+    print(True)
+elif a == "=female":
+    print(False)
+else:
+    print("Lets find out")
+
+input()
+print("--------------------------------------------------------------------------------------------------------")
+print("                                                Casuality profile ")
+print("--------------------------------------------------------------------------------------------------------")
+input()
 calc.average_age_of_casualities(crime_data)
+input()
 calc.sex_of_casuality(crime_data)
-#calc.maximum_number_casualities(crime_data)
+input()
+
+print("--------------------------------------------------------------------------------------------------------")
+print("                                                What causes accidents? ")
+print("--------------------------------------------------------------------------------------------------------")
+input()
 calc.junction_control_with_high_casualities(crime_data)
-#calc.junction_control_with_low_casualities(crime_data)
+input()
 calc.weateher_with_high_casualities(crime_data)
+input()
 calc.lighting_condition(crime_data)
 
-#create a profile of the casuality
+input()
+
+
 
 
 
